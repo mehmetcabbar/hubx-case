7bcf6e55b1b3ca4526cb115551c0ce7a60c17102
Mehmet Cabbar <mehmet.cabbar@missafir.com>
2024-07-09 14:28:47 +0300

    feat/add-custom-font-family

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ export function App() {
   return (
     <>
       <header>
-        <h1 className="text-3xl font-bold underline">HubX Frontend Assignment</h1>
+        <h1 className="text-3xl underline font-sf-pro font-bold">HubX Frontend Assignment</h1>
       </header>
-
       <main className={styles.main}></main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sf-pro': ['SF Pro Display', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
